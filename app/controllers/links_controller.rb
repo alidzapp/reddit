dd7c5e2a97f3ce3c2135a0176a96a7b86daa0437
HassanTC <topcoder52@outlook.com>
2016-06-10 01:51:34 +0200

    add comment and votable

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,6 @@
 class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
-
+  before_filter :authenticate_user!, except: [:index,:show]
   # GET /links
   # GET /links.json
   def index
@@ -14,7 +14,7 @@ class LinksController < ApplicationController
 
   # GET /links/new
   def new
-    @link = Link.new
+    @link = current_user.links.build
   end
 
   # GET /links/1/edit
@@ -24,7 +24,7 @@ class LinksController < ApplicationController
   # POST /links
   # POST /links.json
   def create
-    @link = Link.new(link_params)
+    @link = current_user.links.build(link_params)
 
     respond_to do |format|
       if @link.save
@@ -60,7 +60,16 @@ class LinksController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+  def upvote
+    @link = Link.find(params[:id])
+    @link.upvote_by current_user
+    redirect_to :back
+  end
+  def downvote
+    @link = Link.find(params[:id])
+    @link.downvote_by current_user
+    redirect_to :back
+  end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_link
